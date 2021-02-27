9ec0427a58fad398e4ac6688d433d9ef9568f698
bboykenji0407 <bboy.kenji.flex@gmail.com>
2021-02-27 15:17:31 +0900

    メッセージ送信機能の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,4 @@
 class Message < ApplicationRecord
-  def change
-    create_table :messages do |t|
-      t.string :content
-      t.references :room, foreign_key: true
-      t.references :user, foreign_key: true
-
-      belongs_to :room
-      belongs_to :user
-    end
-  end
+  belongs_to :room
+  belongs_to :user
 end
